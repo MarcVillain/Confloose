6b3b03c1c11cd400c3843b105b6803fd820d6b00
Marc Villain <marc.villain@epita.fr>
2018-03-17 00:05:42 -0700

    [FIX] Fixed spelling of cut

--- 1.sh
+++ 1.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-SCREEN=$(xrandr | grep 'connected' | cur -d' ' -f1);
+SCREEN=$(xrandr | grep 'connected' | cut -d' ' -f1);
 
 while true; do i3-msg 'workspace 1'; done 2>&1 >/dev/null &
 disown
