87b53700a1dca86cc8bbe74a1251c00b067e5645
Marc Villain <marc.villain@epita.fr>
2018-08-20 15:05:44 +0200

    [EDIT] Gifileptic - Improvements

--- 1.sh
+++ 1.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 SCREEN=$(xrandr | grep ' connected' | cut -d' ' -f1);
 
-# Disable input using parameter 'disable'
-DISABLE_INPUT=0
-if ! [ -z '$1' ] && [[ $1 == 'disable' ]]
-then
-    DISABLE_INPUT=1
-fi
+# === VARIABLES === #
+NO_INPUT=0
+FORCE=0
 
+# === FUNCTIONS === #
 function forceWorkspace {
     i3-msg 'workspace 42';
 }
@@ -15,23 +13,20 @@ function forceWorkspace {
 function showImage {
     if ! pgrep -x "firefox" > /dev/null
     then
-        killall -9 firefox;
-        sleep 0.2;
-        firefox --new-window https://raw.githubusercontent.com/MarcVillain/Confloose/master/images/Lock.gif & disown
+        firefox --new-window https://raw.githubusercontent.com/MarcVillain/Confloose/master/images/Lock.gif 2>&1>/dev/null & disown
     fi
 }
 
 function fullScreenFirefox {
-    i3-msg '[title=".*Mozilla Firefox"] fullscreen enable; [title=".*Mozilla Firefox"] move workspace 42';
+    i3-msg '[title=".*Mozilla Firefox"] fullscreen enable; [title=".*Mozilla Firefox"] move workspace 42' 2>&1>/dev/null
 }
 
-function updateGamma {
-    xrandr --output $SCREEN --gamma 1:0.001:0.001;
-    sleep 0.1;
-    xrandr --output $SCREEN --gamma 0.001:1:0.001;
-    sleep 0.1;
-    xrandr --output $SCREEN --gamma 0.001:0.001:1;
-    sleep 0.1;
+function randomGamma {
+    randR=$(($RANDOM%2))
+    randG=$(($RANDOM%2))
+    randB=$(($RANDOM%2))
+
+    xrandr --output $SCREEN --gamma $randR:$randG:$randB 2>&1>/dev/null
 }
 
 function preventMouse {
@@ -46,6 +41,7 @@ function preventKeyboard {
     done < <(xinput | grep Keyboard | tr -d ' ' | tr '\t' ' ' | cut -d' ' -f2 | cut -d'=' -f2)
 }
 
+# === MAIN === #
 function main {
     while true; do
         if [ DISABLE_INPUT = 1 ]
@@ -55,9 +51,32 @@ function main {
         fi
         forceWorkspace;
         showImage;
-        updateGamma;
+        randomGamma;
         fullScreenFirefox;
     done
 }
 
-main 2>&1>/dev/null & disown
+# === OPTIONS === #
+if [ $# == 0 ]
+then
+    main 2>&1>/dev/null
+else
+    for opt in "$@"
+    do
+        if [ opt = "-i" ]
+        then
+            NO_INPUT=1;
+        elif [ opt = "-f" ]
+        then
+            FORCE=1;
+        fi
+    done
+
+    main 2>&1>/dev/null &
+    if [ $FORCE = 1 ]
+    then
+        disown;
+    else
+        echo "Press Ctrl+C to exit.";
+    fi
+fi
