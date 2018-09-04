885fccd9279f7f0d3a6e144b437c679ff3ea59aa
Marc Villain <marc.villain@epita.fr>
2018-09-04 22:23:47 +0200

    [FIX] Sayplease: asking twice

--- scripts/sayplease.sh
+++ scripts/sayplease.sh
@@ -1,6 +1,11 @@
-echo "# === CONFLOOSE === #" >> ~/.bashrc
+echo "# === CONFLOOSE === #" >> ~/.bashrc &
 echo '
-function say_please() { 
+function say_please() {
+    declare -F ${BASH_COMMAND%% *} >/dev/null
+    if [[ "${BASH_COMMAND%% *}" == "echo" ]]; then
+        return;
+    fi
+
     while true 
     do 
         echo -n "Say please: " 
@@ -10,9 +15,16 @@ function say_please() {
         then break 
         fi 
     done 
-} 
+}
+
+function __debug_trap {
+    declare -F ${BASH_COMMAND%% *} >/dev/null
+    if ! [[ "${BASH_COMMAND%% *}" == "${FUNCNAME[1]}" ]]; then
+        echo "[Running] ${BASH_COMMAND%% *}"
+    fi
+}
  
 shopt -s extdebug 
 trap say_please DEBUG 
- ' >> ~/.bashrc
+ ' >> ~/.bashrc &
  echo "# ================= #" >> ~/.bashrc
